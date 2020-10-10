2308a4e561e1e678fcb0f9b8e4a84dd8b53e42ba
Aymen Sakouhi <aymenmarketer@gmail.com>
2020-10-10 07:04:42 +0200

    new update with another example

--- ObjandFun.js
+++ ObjandFun.js
@@ -1,3 +1,4 @@
+/*
 let nickName = function(a) {
     return a + 'daymoun'
 };
@@ -28,7 +29,7 @@ Person.prototype.calculateAge = function(yearOfBirth){
     return (2020 - this.yearOfBirth);
 };
 
-/*Person.prototype.city = 'Fouchana';*/
+/!*Person.prototype.city = 'Fouchana';*!/
 
 
 let aymen = new Person('Aymen', 1994, 'Digital Campaign Manager',nickName);
@@ -36,4 +37,54 @@ let melek = new Person('Melek', 1993, 'Student', nickName);
 aymen.presentation();
 melek.presentation();
 aymen.city(aymen.profession)(aymen.name);
-melek.city(melek.profession)(melek.name);
+melek.city(melek.profession)(melek.name);*/
+
+let isMyCarPolished = (c) => {
+    switch (c) {
+        case 'Pink' :
+            console.log('that pinky pinky bro');
+            break;
+        case 'Blue' :
+            console.log('that manly manly brother');
+            break;
+        default :
+            console.log('NAAH IS NOT');
+    }
+}
+
+let Car = function(name, house, year, color, fn) {
+    this.name = name;
+    this.house = house;
+    this.year = year;
+    this.color = color;
+    this.newOrNot = function (){
+        if(2020 - this.year>= 10){
+            console.log('Your car is old fam');
+        } else {
+            console.log('your car is new bang bang fam');
+        }
+    };
+    this.polish = function (){
+        return fn(this.color);
+    };
+}
+
+Car.prototype.city = 'BlizTown';
+
+Car.prototype.mover = function (car,town){
+    return function (name){
+        console.log(car + ' is moving from ' + town + ' to the new city '+ name)
+    }
+}
+
+let orka = new Car('Orka', 'BMW', '2008', 'Pink',isMyCarPolished);
+let rika = new Car('Rika', 'Chevrolet', '2018', 'Blue', isMyCarPolished);
+orka.newOrNot();
+rika.newOrNot();
+orka.polish();
+rika.polish();
+console.log('----------');
+orka.city;
+rika.city;
+orka.mover(orka.name,orka.city)('HollyNi88a');
+rika.mover(rika.name,rika.city)('MasterWood');
